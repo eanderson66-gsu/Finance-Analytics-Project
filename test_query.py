6083f97b183fcde849712411141e7f381c2e5391
eanderson66-gsu <eanderson66@student.gsu.edu>
2026-02-21 18:48:43 -0500

    Fixed a minor error

--- test_query.py
+++ test_query.py
@@ -2,7 +2,7 @@ import duckdb as db
 
 con = db.connect("setup_example.duckdb")
 
-result = con.sql("SELECT COUNT(*) AS total_customers FROM raw_customers").fetch()
+result = con.sql("SELECT COUNT(*) AS total_customers FROM raw_customers").fetchdf()
 
 print("Connection succesful")
 print(result)
